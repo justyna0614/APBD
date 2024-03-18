c43ec450383da43b5af7a58b7f8357ef4404d10a
s25802 <s25802@pjwstk.edu.pl>
2024-03-18 23:09:12 +0100

    calculating the average

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
@@ -7,5 +8,20 @@ class Program
         Console.WriteLine("Enter your name: ");
         string name = Console.ReadLine()!;
         Console.WriteLine($"Hello, {name}!");
+
+        int[] numbers = { 2, 4, 6};
+        int avg = Avg(numbers);
+        Console.WriteLine($"AVG= {avg}");
+    }
+
+    public static int Avg(int[]arr)
+    {
+        int sum = 0;
+        int size = arr.Length;
+        foreach (int number in arr)
+        {
+            sum +=  number;
+        }
+        return sum / size;
     }
 }
